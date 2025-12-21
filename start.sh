fc1d96a414e9f8971a3e5d79036641d58c4f2a36
kaforshi <aryaintaran09@proton.me>
2025-12-21 23:16:23 +0800

    fix: install deps and run start.sh on container startup

--- start.sh
+++ start.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Install PHP dependencies
+composer install --no-dev --optimize-autoloader
+
+# Install Node dependencies and build assets
+npm install
+npm run build
+
 # Clear Cache
 php artisan optimize:clear
 
